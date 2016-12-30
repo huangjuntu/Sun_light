c974cb759a4f77ae526300ac94577072868b359c
huangjuntu <1721053474@qq.com>
2016-12-30 11:19:07 +0800

    add light

--- js/index.js
+++ js/index.js
@@ -17,18 +17,31 @@ $(function() {
 	function viprank(growthlevel) {
 		lightProgressBar(growthlevel);
 		$('.dbs-point').html('光点：' + growthlevel);
+//		等级一:
 		if(0 < growthlevel && growthlevel <= 100) {
 			$('.dbs-grade').html('lv1');
+			$('.clt').css('background-color','yellow');
+			$('.pt-line p').html("灯1");
+//		等级二
 		} else if(100 < growthlevel && growthlevel <= 300) {
 			$('.dbs-grade').html('lv2');
+			$('.clt').css('background-color','green');
+			$('.pt-line p').html("灯2");
+//		等级三
 		} else if(300 < growthlevel && growthlevel <= 800) {
 			$('.dbs-grade').html('lv3');
+			$('.clt').css('background-color','pink');
+//		等级四
 		} else if(800 < growthlevel && growthlevel <= 1500) {
 			$('.dbs-grade').html('lv4');
+			$('.clt').css('background-color','coral');
+			$('.pt-line p').html("灯王");
+//		等级五
 		} else if(1500 < growthlevel && growthlevel <= 3000) {
 			$('.dbs-grade').html('lv5');
-			$('h3').css('background', 'pink');
-
+			$('.clt').css('background-color','red');
+			$('.pt-line p').html("灯神");
+			
 		}
 	}
 	viprank(1500); //最大为3000光点
